6b005476b1e776beee6cf0869ad7cf13141e3b28
yeonssu <dustn7197@naver.com>
2023-02-04 21:17:12 +0900

    mathAnswer

--- src/powerSet/HouseMeal.java
+++ src/powerSet/HouseMeal.java
@@ -1,6 +1,10 @@
 package powerSet;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
+import static java.util.Arrays.sort;
 
 public class HouseMeal {
     public static void main(String[] args) {
@@ -18,7 +22,29 @@ public class HouseMeal {
         ]
         */
     }
+
     public static ArrayList<String[]> missHouseMeal(String[] sideDishes) {
-        return null;
+        Stack<String> stack = new Stack<>();
+        ArrayList<String[]> result = new ArrayList<>();
+
+        Arrays.sort(sideDishes);
+        result = makeList(stack, 0, sideDishes, result);
+        result.sort((x,y)->Arrays.toString(x).compareTo(Arrays.toString(y)));
+        return result;
+    }
+
+    public static ArrayList<String[]> makeList(Stack<String> stack, int idx, String[] sideDishes, ArrayList<String[]> result) {
+        if (idx >= sideDishes.length) {
+            String[] sub = stack.toArray(new String[0]);
+            result.add(sub);
+            return result;
+        } else {
+            stack.push(sideDishes[idx]);
+            makeList(stack, idx + 1, sideDishes, result);
+
+            stack.pop();
+            makeList(stack, idx + 1, sideDishes, result);
+        }
+        return result;
     }
 }
